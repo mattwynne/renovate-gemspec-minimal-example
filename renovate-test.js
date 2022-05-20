c85dd052da951a60fd759db65f85e9f0ade28860
Matt Wynne <matt@cucumber.io>
2022-05-20 14:38:28 -0700

    Update test to filter out noise

--- renovate-test.js
+++ renovate-test.js
@@ -2,8 +2,13 @@
 const { readFileSync } = require('fs')
 const renovate = require(__dirname + '/renovate.json')
 const regex = new RegExp(renovate.regexManagers[0].matchStrings)
-const lines = readFileSync('my-project.gemspec', 'utf-8').split("\n").map(line => 
-	[line, (regex.exec(line) || { groups: "" }).groups]
+const result = readFileSync('my-project.gemspec', 'utf-8').split("\n").reduce(
+	(lines, line) => {
+		match = regex.exec(line)
+		if (!match) { return lines }
+		return [...lines, [line, JSON.parse(JSON.stringify(regex.exec(line).groups))]]
+	},
+	[]
 )
-console.log(lines)
+console.log(result)
 
